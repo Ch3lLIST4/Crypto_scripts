8ca45c72f58ae0204cf929a003c67c1c6436ddd8
Ch3lLIST4 <56858779+Ch3lLIST4@users.noreply.github.com>
2020-10-07 22:12:38 +0700

    Update hash_calc.py

--- hash_calc.py
+++ hash_calc.py
@@ -5,15 +5,32 @@ import hashlib
 import os
 import sys
 
-def hash_file(filename):
+def hash_file(fn, hash_type):
     """"This function returns the SHA-1 hash
     of the file passed into it"""
 
-    # make a hash object
-    h = hashlib.sha3_512()
+    # make a hash object based on types
+    if hash_type == 'sha1':
+        h = hashlib.sha1()
+    elif hash_type == 'sha224':
+        h = hashlib.sha224()
+    elif hash_type == 'sha256':
+        h = hashlib.sha256()
+    elif hash_type == 'sha384':
+        h = hashlib.sha384()
+    elif hash_type == 'sha512':
+        h = hashlib.sha512()
+    elif hash_type == 'sha3_224':
+        h = hashlib.sha3_224()
+    elif hash_type == 'sha3_256':
+        h = hashlib.sha3_256()
+    elif hash_type == 'sha3_384':
+        h = hashlib.sha3_384()
+    elif hash_type == 'sha3_512':
+        h = hashlib.sha3_512()    
 
     # open file for reading in binary mode
-    with open(filename, 'rb') as file:
+    with open(fn, 'rb') as file:
 
         # loop till the end of the file
         chunk = 0
@@ -25,15 +42,15 @@ def hash_file(filename):
         # return the hex representtion of digest
         return h.hexdigest()
 
-fn = sys.argv[1]
+print(sys.argv)
+hash_type = sys.argv[1]
+fn = sys.argv[2]
 
 if os.path.exists(fn):
     print(os.path.basename(fn))
     # file exists
-    message = hash_file(fn)
+    message = hash_file(fn, hash_type)
     print(message)
 
 else:
     print('Please include an existing file path')
-
-    
